c7d1391e5f99f68126c83ef8c2562e3846213d69
Mukesh Bola <bmkamath2000@gmail.com>
2024-06-27 20:26:35 +0530

    StudentCourse Enrollment

--- 12DJangoStudentCourse/studentcourse/enrollmentapp/forms.py
+++ 12DJangoStudentCourse/studentcourse/enrollmentapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Student
+from .models import Student, Course
 
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['name']
+        fields = '__all__'
--- 12DJangoStudentCourse/studentcourse/enrollmentapp/models.py
+++ 12DJangoStudentCourse/studentcourse/enrollmentapp/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class Student(models.Model):
+class Course(models.Model):
     name = models.CharField(max_length=100)
-
     def __str__(self):
         return self.name
 
-class Course(models.Model):
+
+class Student(models.Model):
     name = models.CharField(max_length=100)
-    students = models.ManyToManyField(Student)
+    courses = models.ManyToManyField(Course)
 
     def __str__(self):
         return self.name
